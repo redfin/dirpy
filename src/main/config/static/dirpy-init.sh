c7c970d2d64d29d50719d3c7f4aaa4f7a5755e4d
Eric Schwimmer <eric.schwimmer@redfin.com>
2014-09-29 12:49:23 -0700

    Added init script.  Added defQuality stanza to config

---
+++ src/main/config/static/dirpy-init.sh
@@ -0,0 +1,85 @@
+
+PATH=/redfin/dirpy:/sbin:/bin:/usr/bin:/usr/sbin
+prog="dirpy"
+conf="/redfin/dirpy/dirpy.conf"
+user="nobody"
+binPath="/redfin/dirpy/dirpy"
+pidFile="/var/run/dirpy/dirpy.pid"
+
+# Source function library.
+. /etc/init.d/functions
+
+if [ "$1" = "status" ] ; then
+	status $prog
+	RETVAL=$?
+	exit $RETVAL
+fi
+
+RETVAL=0
+
+start(){
+    pidDir=$(dirname $pidFile)
+    if [[ ! -d $pidDir ]] ; then
+        mkdir -p $pidDir
+        chown $user $pidDir
+    fi
+
+	echo -n $"Starting $prog: "
+
+	daemon --user=$user $binPath -c $conf
+	RETVAL=$?
+	echo
+	if test $RETVAL = 0 ; then
+		touch /var/lock/subsys/dirpy
+	fi
+	return $RETVAL
+}
+
+stop(){
+	echo -n $"Stopping $prog: "
+	killproc $prog
+	RETVAL=$?
+	echo
+	rm -f /var/lock/subsys/dirpy
+	return $RETVAL
+}
+
+reload(){
+	stop
+	start
+}
+
+restart(){
+	stop
+	start
+}
+
+condrestart(){
+	[ -e /var/lock/subsys/dirpy ] && restart
+	return 0
+}
+
+
+case "$1" in
+    start)
+	start
+	;;
+    stop)
+	stop
+	;;
+    restart)
+	restart
+	;;
+    reload|force-reload)
+	reload
+	;;
+    condrestart|try-restart)
+	condrestart
+	;;
+    *)
+	echo $"Usage: $0 {start|stop|status|restart|condrestart|try-restart|reload|force-reload}"
+	RETVAL=3
+esac
+
+exit $RETVAL
+
